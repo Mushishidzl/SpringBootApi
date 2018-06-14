4790b7008e17cc0cb368bf13965f4fe24df0270b
rain <dzl361258@163.com>
2018-06-14 11:26:08 +0800

    1

---
+++ src/main/java/com/rain/api/common/config/RedisConfig.java
@@ -0,0 +1,11 @@
+package com.rain.api.common.config;
+
+/**
+ * @program: SpringBootApi
+ * @description: Redis 配置
+ * @author: Rain
+ * @create: 2018-06-13 14:38
+ **/
+public class RedisConfig {
+
+}
